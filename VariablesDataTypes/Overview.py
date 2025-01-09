e03b54f25ff485354ff1ff1d357d386e5ee76164
aimansheikh17 <aimansheikh200117@gmail.com>
2025-01-09 21:03:04 +0530

    New added

---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,3 @@
+#1. Python is dynamically typed programming language
+#2. In python while declaring variables, we no need to mention data types
+#3. data types will be assigned by the language based on the value stored in to the variables.
