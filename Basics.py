15c8ab5ae7c0dcfb8278463c24e038c30777e5f1
aimansheikh17 <aimansheikh200117@gmail.com>
2025-01-09 20:55:17 +0530

    Code Added

---
+++ Basics.py
@@ -0,0 +1,14 @@
+#Scripting way of writing code
+print('Hello World')
+
+#Procedural way of writing code
+def message():
+    print('Inside method message: Hello World')
+message()
+
+#Object oriented way of writing code
+class Demo:
+    def instance_method(self):
+        print('Inside Instance method, Hello world')
+d1=Demo()
+d1.instance_method()
